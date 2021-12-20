5ab858faf5e539b1259140177134ae934439ab73
Radmila28 <90576572+Radmila28@users.noreply.github.com>
2021-12-20 23:26:06 +0300

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,15 +1,8 @@
-def prime(n):
-    sqrtn = int(n ** (1/2))
-    k = 0
-    for i in range(1, sqrtn + 1):
-        if n in [1, 2, 3]:
-            k = 1
-            break
-        if n % i != 0:
-            k = 1
-        else:
-            k = 0
-            break
-    if k == 0:
-        return "составное"
-    return "простое"
+def prme(n):
+    if n in [1, 2, 3]:
+        return True
+    sqrtn = int(n ** (1 / 2))
+    for i in range(2, sqrtn + 1):
+        if n % i == 0:
+            return False
+      return True
