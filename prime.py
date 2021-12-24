010b59fd7517cccb8381e88ff01f5903eba96773
Radmila28 <90576572+Radmila28@users.noreply.github.com>
2021-12-24 23:04:48 +0300

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,8 +1,18 @@
-def prme(n):
-    if n in [1, 2, 3]:
-        return True
+def prime(n):
+    if n < 2:
+        return False
     sqrtn = int(n ** (1 / 2))
     for i in range(2, sqrtn + 1):
         if n % i == 0:
             return False
-      return True
+    return True
+
+
+ 
+a = 0
+for i in range(1, 1000001):
+    if prime(i):
+        a += 1
+
+print(a)
+#число простых в диапозоне от 1 до миллиона: 78498
